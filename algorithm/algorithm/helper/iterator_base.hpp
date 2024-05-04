05fa8793d8a9b78da744e32c76749009bdf3acd3
maxshuang <1209541285@qq.com>
2024-05-04 15:26:08 +0800

    add new iterator interface

---
+++ algorithm/algorithm/helper/iterator_base.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iterator>
+
+// std::algorithm style iterator interface
+template <class Tp, class Ptr, class Ref>
+class Iterator : public std::iterator<std::forward_iterator_tag, Tp,
+                                      std::ptrdiff_t, Ptr, Ref>
+{
+public:
+    typedef Iterator<Tp, Ptr, Ref> self_type;
+    virtual ~Iterator() {}
+
+    virtual self_type &operator++() = 0;
+    virtual self_type operator++(int) = 0;
+    virtual bool operator==(const self_type &) const = 0;
+    virtual bool operator!=(const self_type &) const = 0;
+    virtual Ptr operator->() const = 0;
+    virtual Ref operator*() const = 0;
+};
