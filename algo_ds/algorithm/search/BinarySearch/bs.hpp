335a480d9aa1f3a51fb71cb7d5ee5b81ed3d044e
maxshuang <1209541285@qq.com>
2024-08-18 12:11:42 +1000

    add binary search

---
+++ algo_ds/algorithm/search/BinarySearch/bs.hpp
@@ -0,0 +1,68 @@
+#include <vector>
+using namespace std;
+
+int binary_search(std::vector<int>& vec, int target) {
+    int l=0, r=vec.size()-1;
+    while(l<=r) {
+        int mid=(l+r)>>1;
+        if(vec[mid]==target) {
+            return mid;
+        }else if(vec[mid]>target) {
+            r=mid-1;
+        }else {
+            l=mid+1;
+        }
+    }
+
+    return -1;
+}
+
+// the first one that is not smaller than target
+int lower_bound(std::vector<int>& vec, int target) {
+    int l=0, r=vec.size()-1;
+    while(l<=r) {
+        int mid=(l+r)>>1;
+        if(vec[mid]>=target) {
+            r=mid-1;
+        }else {
+            l=mid+1;
+        }
+    }
+
+    // all smaller than target
+    if(l==vec.size()) return -1;
+
+    return l;
+}
+
+// the first one that is larger than target
+int upper_bound(std::vector<int>& vec, int target) {
+    int l=0, r=vec.size()-1;
+    while(l<=r) {
+        int mid=(l+r)>>1;
+        if(vec[mid]>target) {
+            r=mid-1;
+        }else {
+            l=mid+1;
+        }
+    }
+
+    // all smaller than target
+    if(l==vec.size()) return -1;
+
+    return l;
+}
+
+// floor has the same semantic as the lower_bound
+int floor(std::vector<int>& vec, int target) {
+    return lower_bound(vec, target);
+}
+
+// the first one that is not larger than target
+int ceiling(std::vector<int>& vec, int target) {
+    int p=upper_bound(vec, target);
+    // 0, -1, others
+    if(p==0) return -1;
+    else if(p==-1) return vec.size()-1;
+    return p-1;
+}
---
+++ algo_ds/algorithm/search/BinarySearch/test_bs.cpp
@@ -0,0 +1,101 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "bs.hpp"
+
+TEST_CASE("Test binary search", "")
+{
+    std::vector<int> vec{1, 2, 3, 4, 6};
+    
+    SECTION("existed") {
+        REQUIRE(binary_search(vec, 3)==2);
+        REQUIRE(binary_search(vec, 6)==4);
+    }
+
+    SECTION("non-existed") {
+        REQUIRE(binary_search(vec, 5)==-1);
+    }
+}
+
+TEST_CASE("Test upper_bound", "")
+{
+    std::vector<int> vec{1, 2, 3, 3, 4, 6};
+    
+    SECTION("existed in the middle") {
+        REQUIRE(upper_bound(vec, 3)==4);
+        REQUIRE(upper_bound(vec, 2)==2);
+        REQUIRE(upper_bound(vec, 5)==5);
+    }
+
+    SECTION("all larger") {
+        REQUIRE(upper_bound(vec, -1)==0);
+        REQUIRE(upper_bound(vec, -2)==0);
+    }
+
+    SECTION("all smaller") {
+        REQUIRE(upper_bound(vec, 7)==-1);
+        REQUIRE(upper_bound(vec, 9)==-1);
+    }
+}
+
+TEST_CASE("Test lower_bound", "")
+{
+    std::vector<int> vec{1, 2, 3, 3, 4, 6};
+    
+    SECTION("existed in the middle") {
+        REQUIRE(lower_bound(vec, 3)==2);
+        REQUIRE(lower_bound(vec, 2)==1);
+        REQUIRE(lower_bound(vec, 5)==5);
+    }
+
+    SECTION("all larger") {
+        REQUIRE(lower_bound(vec, -1)==0);
+        REQUIRE(lower_bound(vec, -2)==0);
+    }
+
+    SECTION("all smaller") {
+        REQUIRE(lower_bound(vec, 7)==-1);
+        REQUIRE(lower_bound(vec, 9)==-1);
+    }
+}
+
+TEST_CASE("Test floor", "")
+{
+    std::vector<int> vec{1, 2, 3, 3, 4, 6};
+    
+    SECTION("existed in the middle") {
+        REQUIRE(floor(vec, 3)==2);
+        REQUIRE(floor(vec, 2)==1);
+        REQUIRE(floor(vec, 5)==5);
+    }
+
+    SECTION("all larger") {
+        REQUIRE(floor(vec, -1)==0);
+        REQUIRE(floor(vec, -2)==0);
+    }
+
+    SECTION("all smaller") {
+        REQUIRE(floor(vec, 7)==-1);
+        REQUIRE(floor(vec, 9)==-1);
+    }
+}
+
+TEST_CASE("Test ceilling", "")
+{
+    std::vector<int> vec{1, 2, 3, 3, 4, 6};
+    
+    SECTION("existed in the middle") {
+        REQUIRE(ceilling(vec, 3)==3);
+        REQUIRE(ceilling(vec, 2)==1);
+        REQUIRE(ceilling(vec, 5)==4);
+    }
+
+    SECTION("all larger") {
+        REQUIRE(ceilling(vec, -1)==-1;
+        REQUIRE(ceilling(vec, -2)==-1);
+    }
+
+    SECTION("all smaller") {
+        REQUIRE(ceilling(vec, 7)==vec.size()-1);
+        REQUIRE(ceilling(vec, 9)==vec.size()-1);
+    }
+}
